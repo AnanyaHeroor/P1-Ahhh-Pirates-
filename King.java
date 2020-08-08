a77e0c24a76c289c54f7c32e2e9992b796d9044f
Ananya Heroor <47873321+AnanyaHeroor@users.noreply.github.com>
2020-08-08 13:09:22 -0700

    Update King.java

--- King.java
+++ King.java
@@ -1,9 +1,17 @@
+/**
+* This class decodes a new secret message that contains the pirate king's name
+* The pirates' secret message code is different than in Attack.java
+* After converting a letter to an integer, they multiply that value by 3, and then add 21
+* Ex: Argh would be encoded to 216, 363, 330, 333
+*/
 public class King{
     public static void main (String [] args){
+        //decode pirates' secret message
         char one = (char) (237 - 21)/3;
         char two = (char) (354 - 21)/3;
         char three = (char) (354- 21)/3;
         char four = (char) (342- 21)/3;
+        //print decoded pirate king's name
         System.out.println(one + "" + two + "" + three + "" + four + "");
     }
 }
