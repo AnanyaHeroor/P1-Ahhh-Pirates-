cd196aa87c47d84afc91fc26310f21fbe27526db
Ananya Heroor <47873321+AnanyaHeroor@users.noreply.github.com>
2020-08-08 13:05:55 -0700

    Update Attack.java

--- Attack.java
+++ Attack.java
@@ -1,6 +1,14 @@
+/**
+* We have discovered pirate code and the message is:
+* 83,116,97,114,114,114,98,117,99,107,115.
+* This class translates the pirate code and prints out the location at which the pirates will attack
+* 
+*/
 public class Attack {
     public static void main (String [] args){
-      char one = (char) 83;
+        //assign each part of the pirate code to a char variable 
+        //we are translating the numbers to chars to decode
+      char one = (char) 83; 
       char two = (char) 116;
       char three = (char) 97;
       char four = (char) 114;
@@ -11,6 +19,7 @@ public class Attack {
       char nine = (char) 99;
       char ten = (char) 107;
       char eleven = (char) 115;
+        //print the decoded pirate attack location
       System.out.println(one + "" + two + "" + three + "" + four + "" + five + "" + six + "" + seven + "" + eight + "" + nine + "" + ten + eleven);
     }
 }
